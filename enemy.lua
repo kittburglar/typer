1aaad0964740f69c7cf67a3258046a158e067c94
kittburglar <kxayasan@gmail.com>
2016-11-25 12:06:17 -0500

    Handled typos

--- enemy.lua
+++ enemy.lua
@@ -7,6 +7,8 @@ function enemy.create(x, y, width, height, red, green, blue, word)
         width = width,
         height = height,
         word = word,
+        wordCorrectSoFar = " ",
+        wordRemaining = word,
         red = red,
         green = green,
         blue = blue
@@ -21,23 +23,31 @@ function enemy.draw()
     for i,e in ipairs(enemy) do
         love.graphics.setColor(e.red, e.green, e.blue)
         love.graphics.rectangle("fill", e.x, e.y, e.width, e.height)
-        love.graphics.print(e.word, e.x, e.y + e.height)
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.print({{255, 255, 255}, e.wordCorrectSoFar,  {e.red, e.green, e.blue}, e.wordRemaining} , e.x, e.y + e.height)
     end
 end
 
 function enemy.keypressed(key)
     for i,e in ipairs(enemy) do
-        local enemyFirstLetter = e.word:sub(0, 1 )
+        local enemyFirstLetter = e.wordRemaining:sub(0, 1 )
         print("letter pressed was: ",key)
         print("enemy first letter is: ", enemyFirstLetter)
+        print("word corrent so far: ", e.wordCorrectSoFar)
+        print("word remaining are: ", e.wordRemaining)
         if enemyFirstLetter == key then
             print("Letter matched!")
-            local restOfEnemyWord = e.word:sub(2, string.len(e.word))
+            local restOfEnemyWord = e.wordRemaining:sub(2, string.len(e.wordRemaining))
             print(restOfEnemyWord)
-            e.word = restOfEnemyWord
+            -- e.word = restOfEnemyWord
+            e.wordCorrectSoFar = e.wordCorrectSoFar .. key
+            e.wordRemaining = restOfEnemyWord
+        else 
+            e.wordCorrectSoFar = " "
+            e.wordRemaining = e.word
         end
 
-        if e.word == "" then
+        if e.wordRemaining == "" then
             table.remove(enemy, i)
         end
     end
