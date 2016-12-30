8e18922ebbbac1f30089dd7284c2211aaeb44d5b
kittburglar <kxayasan@gmail.com>
2016-12-30 07:59:24 -0500

    Added shading to keyboard buttons

--- keyboard.lua
+++ keyboard.lua
@@ -1,5 +1,6 @@
 keyboard = {}
 local keyPadding = 10
+local keyShadePadding = 5
 
 function keyboard.load()
 	keyboard.height = 216
@@ -25,20 +26,41 @@ end
 function keyboard.draw()
 	love.graphics.setColor(keyboard.red, keyboard.green, keyboard.blue)
 	love.graphics.rectangle("fill", 0, keyboard.y, love.graphics.getWidth(), keyboard.height)
+	-- first button
+	love.graphics.setColor(149,54,57)
+	love.graphics.rectangle("fill", keyboard.firstButton["x"], keyboard.firstButton["y"], keyboard.firstButton["width"], keyboard.firstButton["height"], 10, 10)
 	love.graphics.setColor(213,78,83)
-	love.graphics.rectangle("fill", keyboard.firstButton["x"], keyboard.firstButton["y"], keyboard.firstButton["width"], keyboard.firstButton["height"])
+	love.graphics.rectangle("fill", keyboard.firstButton["x"], keyboard.firstButton["y"], keyboard.firstButton["width"], keyboard.firstButton["height"] - keyShadePadding, 10, 10)
+	-- second button
+	love.graphics.setColor(161,97,48)
+	love.graphics.rectangle("fill", keyboard.secondButton["x"], keyboard.secondButton["y"], keyboard.secondButton["width"], keyboard.secondButton["height"], 10, 10)
 	love.graphics.setColor(231,140,69)
-	love.graphics.rectangle("fill", keyboard.secondButton["x"], keyboard.secondButton["y"], keyboard.secondButton["width"], keyboard.secondButton["height"])
+	love.graphics.rectangle("fill", keyboard.secondButton["x"], keyboard.secondButton["y"], keyboard.secondButton["width"], keyboard.secondButton["height"] - keyShadePadding, 10, 10)
+	-- thrid button
+	love.graphics.setColor(161,137,49)
+	love.graphics.rectangle("fill", keyboard.thirdButton["x"], keyboard.thirdButton["y"], keyboard.thirdButton["width"], keyboard.thirdButton["height"], 10, 10)
 	love.graphics.setColor(231,197,71)
-	love.graphics.rectangle("fill", keyboard.thirdButton["x"], keyboard.thirdButton["y"], keyboard.thirdButton["width"], keyboard.thirdButton["height"])
+	love.graphics.rectangle("fill", keyboard.thirdButton["x"], keyboard.thirdButton["y"], keyboard.thirdButton["width"], keyboard.thirdButton["height"] - keyShadePadding, 10, 10)
+	-- fourth button
+	love.graphics.setColor(129,141,51)
+	love.graphics.rectangle("fill", keyboard.fourthButton["x"], keyboard.fourthButton["y"], keyboard.fourthButton["width"], keyboard.fourthButton["height"], 10, 10)
 	love.graphics.setColor(185,202,74)
-	love.graphics.rectangle("fill", keyboard.fourthButton["x"], keyboard.fourthButton["y"], keyboard.fourthButton["width"], keyboard.fourthButton["height"])
+	love.graphics.rectangle("fill", keyboard.fourthButton["x"], keyboard.fourthButton["y"], keyboard.fourthButton["width"], keyboard.fourthButton["height"] - keyShadePadding, 10, 10)
+	-- fifth button
+	love.graphics.setColor(85,115,152)
+	love.graphics.rectangle("fill", keyboard.fifthButton["x"], keyboard.fifthButton["y"], keyboard.fifthButton["width"], keyboard.fifthButton["height"], 10, 10)
 	love.graphics.setColor(122,166,218)
-	love.graphics.rectangle("fill", keyboard.fifthButton["x"], keyboard.fifthButton["y"], keyboard.fifthButton["width"], keyboard.fifthButton["height"])
+	love.graphics.rectangle("fill", keyboard.fifthButton["x"], keyboard.fifthButton["y"], keyboard.fifthButton["width"], keyboard.fifthButton["height"] - keyShadePadding, 10, 10)
+	-- sixth button
+	love.graphics.setColor(78,134,123)
+	love.graphics.rectangle("fill", keyboard.sixthButton["x"], keyboard.sixthButton["y"], keyboard.sixthButton["width"], keyboard.sixthButton["height"], 10, 10)
 	love.graphics.setColor(112,192,177)
-	love.graphics.rectangle("fill", keyboard.sixthButton["x"], keyboard.sixthButton["y"], keyboard.sixthButton["width"], keyboard.sixthButton["height"])
+	love.graphics.rectangle("fill", keyboard.sixthButton["x"], keyboard.sixthButton["y"], keyboard.sixthButton["width"], keyboard.sixthButton["height"] - keyShadePadding, 10, 10)
+	-- seventh button
+	love.graphics.setColor(136,105,151)
+	love.graphics.rectangle("fill", keyboard.seventhButton["x"], keyboard.seventhButton["y"], keyboard.seventhButton["width"], keyboard.seventhButton["height"], 10, 10)
 	love.graphics.setColor(195,151,216)
-	love.graphics.rectangle("fill", keyboard.seventhButton["x"], keyboard.seventhButton["y"], keyboard.seventhButton["width"], keyboard.seventhButton["height"])
+	love.graphics.rectangle("fill", keyboard.seventhButton["x"], keyboard.seventhButton["y"], keyboard.seventhButton["width"], keyboard.seventhButton["height"] - keyShadePadding, 10, 10)
 	love.graphics.setColor(255, 255, 255)
 
 
